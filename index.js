1d2de303623bac03e9f1e00ab35c8e0f80ebe80d
anik1606@gmail.com <anik1606@gmail.com>
2018-09-13 21:12:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(counter) {
   var arr = [];
-  var counter = 0;
+  var counter = 1;
   do { 
     arr.push("I love the Beatles!");
     counter++;
